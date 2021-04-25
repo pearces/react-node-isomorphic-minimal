29ceb9d6585721663a666d7bd4859c89eb467e94
Stephen Pearce <stephen.r.pearce@gmail.com>
2021-04-25 13:59:09 -0400

    Changed webpack client target to use browserslist, added config and updated stats, added watcher config to webpack for dev and nodemon to restart the dev server, minor rework of build/run scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,51 @@ const nodeExternals = require('webpack-node-externals');
 
 const distPath = path.join(__dirname, 'dist');
 
-const moduleConfig = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      resolve: {
-        extensions: ['.js', '.jsx']
-      },
-      use: {
-        loader: 'babel-loader'
+const common = {
+  ...(process.env.NODE_ENV === 'development' && {
+    watch: true,
+    watchOptions: {
+      ignored: /node_modules/
+    },
+    devtool: 'eval-source-map'
+  }),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx']
+        },
+        use: {
+          loader: 'babel-loader'
+        }
       }
-    }
-  ]
+    ]
+  }
 };
 
 const clientConfig = {
+  ...common,
+  target: 'browserslist',
   entry: {
     bundle: ['./src/client.js']
   },
   output: {
     path: distPath,
     filename: '[name].js'
-  },
-  module: moduleConfig
+  }
 };
 
 const serverConfig = {
+  ...common,
   target: 'node',
   externals: [nodeExternals()],
   entry: './src/server.js',
   output: {
     path: distPath,
     filename: 'server.js'
-  },
-  module: moduleConfig
+  }
 };
 
 module.exports = [clientConfig, serverConfig];
