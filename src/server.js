ed101266146ea9bb8441a217cb764292f6636002
Stephen Pearce <stephen.r.pearce@gmail.com>
2021-05-12 23:55:29 -0400

    Reworked server.js to only fetch assets list on start rather than each request, added missing doctype for compliance, minor cleanup, bumped dependencies to latest versions

--- src/server.js
+++ src/server.js
@@ -13,37 +13,35 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-
 const { PORT: port = DEFAULT_PORT, NODE_ENV } = process.env;
 
 let inlineCss;
+let assets = [];
+const cacheControl = NODE_ENV === 'development' ? 'no-cache' : 'max-age=300';
 
 const getAssets = () => {
   const manifest = fs.readFileSync(path.join(__dirname, 'manifest.json'), { encoding: 'utf-8' });
-  const assets = JSON.parse(manifest);
+  const allAssets = JSON.parse(manifest);
 
-  return Object.values(assets);
+  return Object.values(allAssets);
 };
 
-const getAssetType = (assets = [], ext = '') => assets.filter((asset) => RegExp(`.${ext}`).test(asset));
-
 app.get('/', (req, res) => {
-  const assets = getAssets();
+  const getAssetType = (ext) => assets.filter((asset) => RegExp(`.${ext}`).test(asset));
+
   const content = ReactDOMServer.renderToString(
     <Html
       title={APP_NAME}
-      stylesheets={getAssetType(assets, 'css')}
-      scripts={getAssetType(assets, 'js')}
+      stylesheets={getAssetType('css')}
+      scripts={getAssetType('js')}
       inlineCss={inlineCss}
     >
       <App />
     </Html>
   );
 
-  if (req.method === 'GET') {
-    res.set('Cache-Control', NODE_ENV === 'development' ? 'no-cache' : 'max-age=300');
-  }
-  res.status(200).send(content);
+  if (req.method === 'GET') res.set('Cache-Control', cacheControl);
+  res.status(200).send(`<!DOCTYPE html>${content}`);
 });
 
 app.use([/^\/static\/server\.js/, '/static'], express.static(path.join(__dirname)));
@@ -53,6 +51,8 @@ app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
 app.listen(port, () => {
   const inlineCssPath = path.join(__dirname, INLINE_CSS_FILE);
   if (fs.existsSync(inlineCssPath)) inlineCss = fs.readFileSync(inlineCssPath, { encoding: 'utf-8' });
+
+  assets = getAssets();
 
   console.log(`App listening on localhost:${port}`); // eslint-disable-line no-console
 });
