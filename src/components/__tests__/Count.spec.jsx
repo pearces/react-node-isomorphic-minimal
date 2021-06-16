051bd4777e75c2f3cc8eeb571f27191dfb38cbbd
Stephen Pearce <stephen.r.pearce@gmail.com>
2021-06-15 23:57:12 -0400

    Added fetch action unit tests to Count component

--- src/components/__tests__/Count.spec.jsx
+++ src/components/__tests__/Count.spec.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from 'reducers';
+import { CALL_STATUS } from 'reducers/date';
 import Count from '../Count';
+import fetchMiddleware from '../../fetchMiddleware';
+
+const { PENDING, COMPLETE, FAILED } = CALL_STATUS;
+
+let mockApiSuccess = true;
+let mockDate;
+const mockApiError = 'API call failed';
+const mockFetch = jest.fn(() => Promise.resolve({
+  json: () => {
+    mockDate = Date.now();
+    return mockApiSuccess ? Promise.resolve(mockDate) : Promise.reject(Error(mockApiError));
+  }
+}));
 
 describe('Count', () => {
   let useEffect;
@@ -14,7 +28,10 @@ describe('Count', () => {
     useEffect = jest.spyOn(React, 'useEffect');
     useEffect.mockClear();
 
-    store = createStore(rootReducer, { count: 0, date: {} });
+    global.fetch = mockFetch;
+    fetch.mockClear();
+
+    store = createStore(rootReducer, { count: 0, date: {} }, applyMiddleware(fetchMiddleware));
   });
 
   it('matches component snapshot', () => {
@@ -44,5 +61,41 @@ describe('Count', () => {
     expect(section.find('p').text()).toEqual(`The button has been clicked ${expectedCount} times.`);
     expect(useEffect).toHaveBeenLastCalledWith(expect.any(Function), [0]);
     expect(document.title.endsWith('(0)')).toBe(true);
+  });
+
+  it('handles component API call button clicks with pending response', (done) => {
+    global.fetch = () => new Promise(() => {});
+    const app = mount(<Provider store={store}><Count /></Provider>);
+    const section = app.find('section').at(2);
+    section.find('button').simulate('click');
+    setTimeout(() => {
+      expect(section.find('p').text().includes(PENDING)).toEqual(true);
+      done();
+    }, 0);
+  });
+
+  it('handles component API call button clicks with successful response', (done) => {
+    const app = mount(<Provider store={store}><Count /></Provider>);
+    const section = app.find('section').at(2);
+    section.find('button').simulate('click');
+    setTimeout(() => {
+      const text = section.find('p').text();
+      expect(text.includes(COMPLETE)).toEqual(true);
+      expect(text.includes(Date(mockDate))).toEqual(true);
+      done();
+    }, 0);
+  });
+
+  it('handles component API call button clicks with failed response', (done) => {
+    mockApiSuccess = false;
+    const app = mount(<Provider store={store}><Count /></Provider>);
+    const section = app.find('section').at(2);
+    section.find('button').simulate('click');
+    setTimeout(() => {
+      const text = section.find('p').text();
+      expect(text.includes(FAILED)).toEqual(true);
+      expect(text.includes(mockApiError)).toEqual(true);
+      done();
+    }, 0);
   });
 });
--- src/reducers/date.js
+++ src/reducers/date.js
@@ -2,14 +2,22 @@ import { GET_DATE } from 'actions/date';
 
 export const DEFAULT_STATE = {};
 
+export const CALL_STATUS = {
+  PENDING: 'pending',
+  COMPLETE: 'complete',
+  FAILED: 'failed'
+};
+
 export default (state = DEFAULT_STATE, action) => {
+  const { PENDING, COMPLETE, FAILED } = CALL_STATUS;
+
   switch (action.type) {
     case `${GET_DATE}_REQUESTED`:
-      return { ...state, status: 'pending', message: '' };
+      return { ...state, status: PENDING, message: '' };
     case `${GET_DATE}_SUCCESS`:
-      return { ...state, status: 'complete', message: Date(action.payload) };
+      return { ...state, status: COMPLETE, message: Date(action.payload) };
     case `${GET_DATE}_FAILED`:
-      return { ...state, status: 'failed', message: action.error.message };
+      return { ...state, status: FAILED, message: action.error.message };
     default:
       return state;
   }
